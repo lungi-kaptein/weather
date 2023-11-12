fc57840cde82e063100eb2464dd9e33301859128
lungi-kaptein <lungikaptein@gmail.com>
2023-11-13 00:35:38 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHtml = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "6abedda1ad6224fbft004155f03odf27";
+  let apiUrl =
+    " https://api.shecodes.io/weather/v1/current?query=${city}&key=6abedda1ad6224fbft004155f03odf27&units=metric ";
+  axious.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
